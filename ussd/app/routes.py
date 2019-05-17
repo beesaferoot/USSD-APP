28deaec18065df8d36c8a9a7e2410330cca79fb6
hikeobinna <hikenike6@gmail.com>
2019-05-17 03:12:07 +0100

    Update to routes.py

--- ussd/app/routes.py
+++ ussd/app/routes.py
@@ -10,7 +10,7 @@ MENU_OPTIONS = {
     "": ["CON Personal Helper\n", "1. Get Service Codes.\n"],
     "1": ["CON Select Options\n", "1. Recharge.\n", "2. Check Balance.\n"],
     "1*1": ["CON Select Network\n", "1. Glo.\n", "2. Mtn.\n"],
-    "1*1*1": ["END Recharge without Data Bonus = *123*PIN#\n", "Recharge with Data Bonus = *222*PIN#\n"],
+    "1*1*1": ["END Recharge without Data Bonus = *123*PIN#\n", "Recharge with Data Bonus = *223*PIN#\n"],
     "1*1*2": ["END Recharge = *555*PIN#\n"],
     "1*2":["CON Select Network\n", "1. Glo.\n", "2. Mtn.\n"],
     "1*2*1": ["END Check Balance = #124#\n"],
